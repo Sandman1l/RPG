363c41833f17a6c94c87412ba5ac1085f0c33b52
LuisSFU <Saloch_1s@hotmail.com>
2019-03-22 11:00:04 -0600

    Simple Sort by Ag

--- Assets/Scripts/TurnHelper.cs
+++ Assets/Scripts/TurnHelper.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 public class TurnHelper : MonoBehaviour 
 {
     protected List<Unit> Units;
     protected void AgilitySort()
     {
-        
+        Units = new List<Unit>(Units.OrderByDescending(u => u.ag));
     }
 }
--- Assets/Scripts/TurnSystem.cs
+++ Assets/Scripts/TurnSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 // Turn Helper inherits the MonoBehaviour class
@@ -7,17 +8,26 @@ public class TurnSystem : TurnHelper
 {	
 	// Use this for initialization
 	void Start () {
+		
+		if (Units.Count != 0)
+		{
+			Units.Clear();
+		}
+		
 		Units = new List<Unit>();
 		//Arrays for player and enemy units
 		var playerUnits = GameObject.FindGameObjectsWithTag("Player");
 		var enemyUnits = GameObject.FindGameObjectsWithTag("Enemy");
-		
-		foreach (GameObject player in playerUnits) {
-			var currentUnit = player.GetComponent<Unit> ();
-			currentUnit.Turn (0);
-			Units.Add (currentUnit);
+
+		foreach (var player in playerUnits)
+		{
+			var currentUnit = player.GetComponent<Unit>();
+			currentUnit.Turn(0);
+			Units.Add(currentUnit);
 		}
-		foreach (GameObject enemy in enemyUnits) {
+
+		foreach (var enemy in enemyUnits)
+		{
 			var currentUnit = enemy.GetComponent<Unit> ();
 			currentUnit.Turn (0);
 			Units.Add (currentUnit);
